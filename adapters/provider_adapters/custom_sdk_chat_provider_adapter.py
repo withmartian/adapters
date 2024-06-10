692ee3d93113c3329d2196e9b23c1ea10c05311a
luka sefashvili <luksepashvili@gmail.com>
2024-06-10 14:32:19 +0400

    refactor: custom sdk chat provider

--- adapters/provider_adapters/custom_sdk_chat_provider_adapter.py
+++ adapters/provider_adapters/custom_sdk_chat_provider_adapter.py
@@ -1,17 +1,21 @@
 import re
-from typing import Optional, Pattern
+from typing import Optional
 
 from adapters.abstract_adapters.openai_sdk_chat_adapter import OpenAISDKChatAdapter
 from adapters.abstract_adapters.provider_adapter_mixin import ProviderAdapterMixin
 
-API_KEY_NAME = ""
-API_KEY_PATTERN = re.compile(r".*")
+API_KEY_NAME = "CUSTOM_API_KEY"
+API_KEY_PATTERN = re.compile(r"^sk-[a-zA-Z0-9\-]+$")
 
 
-class CustomOpenAISDKChatProviderAdapter(ProviderAdapterMixin, OpenAISDKChatAdapter):
+class CustomAISDKChatProviderAdapter(ProviderAdapterMixin, OpenAISDKChatAdapter):
     def __init__(self, base_url: str):
         self.base_url = base_url
         super().__init__()
+
+    @staticmethod
+    def get_api_key_pattern() -> re.Pattern:
+        return API_KEY_PATTERN
 
     def get_custom_sdk_url(self) -> Optional[str]:
         return self.base_url
@@ -23,7 +27,3 @@ class CustomOpenAISDKChatProviderAdapter(ProviderAdapterMixin, OpenAISDKChatAdap
     @staticmethod
     def get_api_key_name() -> str:
         return API_KEY_NAME
-
-    @staticmethod
-    def get_api_key_pattern() -> Pattern:
-        return API_KEY_PATTERN
