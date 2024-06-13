ed16e9650bb17dd688caf075fc98c7d1f6502663
luka sefashvili <luksepashvili@gmail.com>
2024-06-13 21:29:07 +0400

    undo commit

--- adapters/provider_adapters/openrouter_sdk_chat_provider_adapter.py
+++ adapters/provider_adapters/openrouter_sdk_chat_provider_adapter.py
@@ -68,6 +68,12 @@ MODELS = [
         context_length=8192,
         vendor_name="pygmalionai",
     ),
+    OpenRouterModel(
+        name="mythomax-l2-13b",
+        cost=Cost(prompt=0.27e-6, completion=0.27e-6),
+        context_length=4096,
+        vendor_name="gryphe",
+    ),
 ]
 
 
