f9623b23c20b8ba50dd08e0ae27940f8f38c2700
luka sefashvili <luksepashvili@gmail.com>
2024-06-14 20:17:34 +0400

    fix: stream tests

--- tests/adapters/test_execute_stream.py
+++ tests/adapters/test_execute_stream.py
@@ -1,128 +1,66 @@
-# import pytest
+import json
 
-# from adapters.adapter_factory import AdapterFactory
-# from adapters.types import ConversationRole
-# from tests.adapters.utils.contants import MODEL_PATHS
-# from tests.utils import (
-#     SIMPLE_CONVERSATION_USER_ONLY,
-#     get_choices_from_vcr,
-# )
+import pytest
 
-# @pytest.mark.parametrize("model_name", MODEL_NAMES)
-# @pytest.mark.vcr
-# def test_sync_execute_streaming(vcr, model_name):
-#     adapter = AdapterFactory.get_adapter_by_path(model_name)
-
-#     assert adapter is not None
+from adapters.adapter_factory import AdapterFactory
+from tests.adapters.utils.contants import MODEL_PATHS
+from tests.utils import SIMPLE_CONVERSATION_USER_ONLY
 
 
-#     if adapter.get_model().supports_streaming is False:
-#         return
+@pytest.mark.parametrize("model_name", MODEL_PATHS)
+@pytest.mark.vcr
+def test_sync_execute_streaming(model_name):
+    adapter = AdapterFactory.get_adapter_by_path(model_name)
 
-#     adapter_response = adapter.execute_sync(
-#         adapter.convert_to_input(SIMPLE_CONVERSATION_USER_ONLY),
-#         stream=True,
-#     )
+    assert adapter is not None
 
-#     chunks = [
-#         json.loads(data_chunk[6:].strip()) for data_chunk in adapter_response.response
-#     ]
+    if adapter.get_model().supports_streaming is False:
+        return
 
-#     response = "".join(
-#         [
-#             chunk["choices"][0]["delta"]["content"]
-#             for chunk in chunks
-#             if chunk["choices"][0]["delta"]["content"]
-#         ]
-#     )
-#     assert len(response) > 0
+    adapter_response = adapter.execute_sync(
+        adapter.convert_to_input(SIMPLE_CONVERSATION_USER_ONLY),
+        stream=True,
+    )
 
+    chunks = [
+        json.loads(data_chunk[6:].strip()) for data_chunk in adapter_response.response
+    ]
 
-# @pytest.mark.parametrize("model_name", MODEL_NAMES)
-# @pytest.mark.vcr
-# async def test_async_execute_streaming(model_name):
-#     adapter = AdapterFactory.get_adapter_by_path(model_name)
-
-#     assert adapter is not None
-
-
-#     if adapter.get_model().supports_streaming is False:
-#         return
-
-#     adapter_response = await adapter.execute_async(
-#         adapter.convert_to_input(SIMPLE_CONVERSATION_USER_ONLY),
-#         stream=True,
-#     )
-
-#     chunks = [
-#         json.loads(data_chunk[6:].strip())
-#         async for data_chunk in adapter_response.response
-#     ]
-
-#     response = "".join(
-#         [
-#             chunk["choices"][0]["delta"]["content"]
-#             for chunk in chunks
-#             if chunk["choices"][0]["delta"]["content"]
-#         ]
-#     )
-#     assert len(response) > 0
+    response = "".join(
+        [
+            chunk["choices"][0]["delta"]["content"]
+            for chunk in chunks
+            if chunk["choices"][0]["delta"]["content"]
+        ]
+    )
+    assert len(response) > 0
 
 
-# # @pytest.mark.parametrize("model_name", ANTHROPIC_CHAT_MODELS)
-# # @pytest.mark.vcr
-# # async def test_async_execute_streaming(
-# #     vcr, model_name  # pylint: disable=unused-argument
-# # ):
-# #     adapter = AdapterFactory.get_adapter_by_path(model_name)
+@pytest.mark.parametrize("model_name", MODEL_PATHS)
+@pytest.mark.vcr
+async def test_async_execute_streaming(model_name):
+    adapter = AdapterFactory.get_adapter_by_path(model_name)
+    assert adapter is not None
 
-# #     if not adapter.get_model().supports_streaming:
-# #         return
+    if adapter.get_model().supports_streaming is False:
+        return
 
-# #     adapter_response = await adapter.execute_async(
-# #         adapter.convert_to_input(SIMPLE_CONVERSATION_USER_ONLY),
-# #         stream=True,
-# #     )
+    adapter_response = await adapter.execute_async(
+        adapter.convert_to_input(SIMPLE_CONVERSATION_USER_ONLY),
+        stream=True,
+    )
 
-# #     chunks = [
-# #         json.loads(data_chunk[6:].strip())
-# #         async for data_chunk in adapter_response.response
-# #     ]
+    chunks = [
+        json.loads(data_chunk[6:].strip())
+        async for data_chunk in adapter_response.response
+    ]
 
-# #     response = "".join(
-# #         [
-# #             chunk["choices"][0]["delta"]["content"]
-# #             for chunk in chunks
-# #             if chunk["choices"][0]["delta"]["content"]
-# #         ]
-# #     )
+    response = "".join(
+        [
+            chunk["choices"][0]["delta"]["content"]
+            for chunk in chunks
+            if chunk["choices"][0]["delta"]["content"]
+        ]
+    )
 
-# #     assert len(response) > 0
-
-
-# # @pytest.mark.parametrize("model_name", ANTHROPIC_CHAT_MODELS)
-# # @pytest.mark.vcr
-# # def test_sync_execute_streaming(vcr, model_name):  # pylint: disable=unused-argument
-# #     adapter = AdapterFactory.get_adapter_by_path(model_name)
-
-# #     if not adapter.get_model().supports_streaming:
-# #         return
-
-# #     adapter_response = adapter.execute_sync(
-# #         adapter.convert_to_input(SIMPLE_CONVERSATION_USER_ONLY),
-# #         stream=True,
-# #     )
-
-# #     chunks = [
-# #         json.loads(data_chunk[6:].strip()) for data_chunk in adapter_response.response
-# #     ]
-
-# #     response = "".join(
-# #         [
-# #             chunk["choices"][0]["delta"]["content"]
-# #             for chunk in chunks
-# #             if chunk["choices"][0]["delta"]["content"]
-# #         ]
-# #     )
-
-# #     assert len(response) > 0
+    assert len(response) > 0
