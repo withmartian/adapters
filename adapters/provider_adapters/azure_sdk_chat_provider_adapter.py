f097c13bf7038f30d3012794b07f6043244c6a46
luka sefashvili <luksepashvili@gmail.com>
2024-06-20 22:06:14 +0400

    feat: add azure provider

--- adapters/provider_adapters/azure_sdk_chat_provider_adapter.py
+++ adapters/provider_adapters/azure_sdk_chat_provider_adapter.py
@@ -3,11 +3,11 @@ from typing import Pattern
 
 from adapters.abstract_adapters.openai_sdk_chat_adapter import OpenAISDKChatAdapter
 from adapters.abstract_adapters.provider_adapter_mixin import ProviderAdapterMixin
-from adapters.types import Model
+from adapters.types import Cost, Model
 
 VENDOR_NAME = "azure"
 PROVIDER_NAME = "azure"
-BASE_URL = ""
+BASE_URL = "https://martiantest.openai.azure.com/"
 API_KEY_NAME = "AZURE_API_KEY"
 API_KEY_PATTERN = re.compile(r".*")
 
@@ -17,15 +17,27 @@ class AzureModel(Model):
     supports_functions: bool = True
     supports_tools: bool = True
     supports_n: bool = True
-    supports_json: bool = True
-    vendor_name: str = VENDOR_NAME
+    supports_json_output: bool = True
+    supports_json_content: bool = True
+    vendor_name: str = PROVIDER_NAME
     provider_name: str = PROVIDER_NAME
+
+
+MODELS = [
+    AzureModel(
+        name="gpt-35-turbo-16k",
+        cost=Cost(prompt=1.0e-6, completion=2.0e-6),
+        context_length=16385,
+        completion_length=16385,
+    )
+]
 
 
 class AzureSDKChatProviderAdapter(ProviderAdapterMixin, OpenAISDKChatAdapter):
     @staticmethod
     def get_supported_models():
-        return []
+        print("heheee")
+        return MODELS
 
     @staticmethod
     def get_provider_name() -> str:
