5e58941453faae0a1a1dc6d61bb6302ea958603c
lukesamkharadze <lukesamkharadze@icloud.com>
2024-09-04 13:20:27 -0700

    feat: add ai21 models

---
+++ adapters/provider_adapters/ai21_sdk_chat_provider_adapter.py
@@ -0,0 +1,61 @@
+import re
+from typing import Pattern
+
+from adapters.abstract_adapters.openai_sdk_chat_adapter import OpenAISDKChatAdapter
+from adapters.abstract_adapters.provider_adapter_mixin import ProviderAdapterMixin
+from adapters.types import Cost, Model
+
+PROVIDER_NAME = "ai21"
+BASE_URL = "https://api.ai21.com/studio/v1"
+API_KEY_NAME = "AI21_API_KEY"
+API_KEY_PATTERN = re.compile(r".*")
+
+
+class AI21Model(Model):
+    supports_streaming: bool = True
+    supports_empty_content: bool = False
+    supports_json_output: bool = True
+    supports_json_content: bool = True
+    supports_tools: bool = True
+    supports_functions: bool = False
+    supports_n: bool = True
+    provider_name: str = PROVIDER_NAME
+    vendor_name: str = PROVIDER_NAME
+
+    def _get_api_path(self) -> str:
+        return f"{self.name}"
+
+
+MODELS: list[AI21Model] = [
+    AI21Model(
+        name="jamba-1.5-mini",
+        cost=Cost(prompt=0.2e-6, completion=0.4e-6),
+        context_length=256000,
+    ),
+    AI21Model(
+        name="jamba-1.5-large",
+        cost=Cost(prompt=2.0e-6, completion=8.0e-6),
+        context_length=256000,
+    ),
+]
+
+
+class AI21SDKChatProviderAdapter(ProviderAdapterMixin, OpenAISDKChatAdapter):
+    @staticmethod
+    def get_supported_models():
+        return MODELS
+
+    @staticmethod
+    def get_provider_name() -> str:
+        return PROVIDER_NAME
+
+    def get_base_sdk_url(self) -> str:
+        return BASE_URL
+
+    @staticmethod
+    def get_api_key_name() -> str:
+        return API_KEY_NAME
+
+    @staticmethod
+    def get_api_key_pattern() -> Pattern:
+        return API_KEY_PATTERN
