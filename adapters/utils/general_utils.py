ea2ae195a11ba2135b4f676eaec26ba61db412af
lukesamkharadze <lukesamkharadze@icloud.com>
2024-09-25 17:58:55 -0700

    refactor: remove load_request_body_with_additional_params

--- adapters/utils/general_utils.py
+++ adapters/utils/general_utils.py
@@ -9,21 +9,6 @@ from adapters.types import Cost, RequestBody
 EMPTY_CONTENT = '""'
 
 
-def load_request_body_with_additional_params(
-    request_body: RequestBody,
-    temperature: Optional[float] = None,
-    max_tokens: Optional[int] = None,
-    **kwargs,
-) -> RequestBody:
-    for key, value in kwargs.items():
-        request_body.setdefault(key, value)
-    if temperature is not None:
-        request_body.setdefault("temperature", temperature)
-    if max_tokens is not None:
-        request_body.setdefault("max_tokens", max_tokens)
-    return request_body
-
-
 def delete_none_values(dictionary: dict):
     if isinstance(dictionary, list):
         return [delete_none_values(e) for e in dictionary]
