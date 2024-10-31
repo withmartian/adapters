4cf9dbe57a5f909a989342097499f448af096b9a
Luka Samkharadze <lukesamkharadze@icloud.com>
2024-10-31 12:31:49 -0700

    fix: refactor openai custom adapter

--- adapters/custom_sdk_chat_provider_adapter.py
+++ adapters/custom_sdk_chat_provider_adapter.py
@@ -1,18 +1,7 @@
 from adapters.abstract_adapters.openai_sdk_chat_adapter import OpenAISDKChatAdapter
 
 
-class CustomAISDKChatProviderAdapter(OpenAISDKChatAdapter):
-    def __init__(self, base_url: str):
-        self.base_url = base_url
-        super().__init__()
-
+class CustomOpenAISDKChatProviderAdapter(OpenAISDKChatAdapter):
     @staticmethod
     def get_supported_models():
         return []
-
-    def get_base_sdk_url(self) -> str:
-        return self.base_url
-
-    @staticmethod
-    def get_api_key_name() -> str:
-        return ""
