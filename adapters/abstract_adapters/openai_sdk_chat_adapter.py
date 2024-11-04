d18061ab9e4a6974412b91f973f2d4a0bb515dc4
Luka Samkharadze <lukesamkharadze@icloud.com>
2024-11-04 14:07:15 -0800

    feat: add network configuration support

--- adapters/abstract_adapters/openai_sdk_chat_adapter.py
+++ adapters/abstract_adapters/openai_sdk_chat_adapter.py
@@ -1,10 +1,17 @@
 from typing import Callable
 
+from httpx import AsyncClient, Client, Limits, Timeout
 from openai import AsyncOpenAI, OpenAI
 from openai.types.chat import ChatCompletion
 from openai.types.chat.chat_completion_chunk import ChatCompletionChunk
 
 from adapters.abstract_adapters.sdk_chat_adapter import SDKChatAdapter
+from adapters.constants import (
+    HTTP_CONNECT_TIMEOUT,
+    HTTP_TIMEOUT,
+    MAX_CONNECTIONS_PER_PROCESS,
+    MAX_KEEPALIVE_CONNECTIONS_PER_PROCESS,
+)
 from adapters.types import (
     AdapterChatCompletion,
     AdapterChatCompletionChunk,
@@ -27,6 +34,13 @@ class OpenAISDKChatAdapter(SDKChatAdapter[OpenAI, AsyncOpenAI]):
             base_url=base_url,
             api_key=api_key,
             max_retries=0,
+            http_client=Client(
+                limits=Limits(
+                    max_connections=MAX_CONNECTIONS_PER_PROCESS,
+                    max_keepalive_connections=MAX_KEEPALIVE_CONNECTIONS_PER_PROCESS,
+                ),
+                timeout=Timeout(timeout=HTTP_TIMEOUT, connect=HTTP_CONNECT_TIMEOUT),
+            ),
         )
 
     def _create_client_async(self, base_url: str, api_key: str) -> AsyncOpenAI:
@@ -34,6 +48,13 @@ class OpenAISDKChatAdapter(SDKChatAdapter[OpenAI, AsyncOpenAI]):
             base_url=base_url,
             api_key=api_key,
             max_retries=0,
+            http_client=AsyncClient(
+                limits=Limits(
+                    max_connections=MAX_CONNECTIONS_PER_PROCESS,
+                    max_keepalive_connections=MAX_KEEPALIVE_CONNECTIONS_PER_PROCESS,
+                ),
+                timeout=Timeout(timeout=HTTP_TIMEOUT, connect=HTTP_CONNECT_TIMEOUT),
+            ),
         )
 
     def _extract_response(
